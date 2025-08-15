ce7b28d65af459d254f50831941ad307de0138c7
Georgy <ggesse@softwaresecured.com>
2025-08-15 09:57:43 -0400

    finally done with the help

--- cmd.go
+++ cmd.go
@@ -2,7 +2,7 @@ package go_cmd
 
 import (
 	"fmt"
-	// "maps"
+	"os"
 	"reflect"
 	"strings"
 )
@@ -19,6 +19,8 @@ import (
 // Global consts
 const CMD_MAIN string = "main"
 
+type CMD_HELP_TYPE bool
+
 // Helper function to easily get runtime typing since types are not first class for some reason...
 func CmdTypeOf[T any]() reflect.Type {
 	var zero T
@@ -29,8 +31,8 @@ func CmdTypeOf[T any]() reflect.Type {
 // Pass in the slice of listCmd that doesn't include the command name
 // ex: for command "foo --bar=test", with listCmd [foo, --bar, test]
 // --> pass in [--bar, test] with the correct specificCmd using list[1:]
-func parseParams(listCmd []string, specificCmd CmdCommand) (map[string]string, error) {
-	parsedParams := make(map[string]string)
+func parseParams(listCmd []string, specificCmd CmdCommand) (map[string]CmdParam, error) {
+	parsedParams := make(map[string]CmdParam)
 	for it := 0; it < len(listCmd); it += 1 {
 		// if the paramName doesn't exist in command description, return err
 		word := listCmd[it]
@@ -40,9 +42,18 @@ func parseParams(listCmd []string, specificCmd CmdCommand) (map[string]string, e
 		}
 		// can rename the variable, know that it is a valid param name
 		parameterName := word
+		// if help flag encountered, exit early and print the help message
+		if parameterType.ParamType == CmdTypeOf[CMD_HELP_TYPE]() {
+			parsedParams[parameterName] = CmdParam{
+				ParamValue: "true",
+				ParamType: CmdTypeOf[CMD_HELP_TYPE](),
+			}
 		// boolean params (flags) don't need values
-		if parameterType.ParamType == CmdTypeOf[bool]() {
-			parsedParams[parameterName] = "true"
+		} else if parameterType.ParamType == CmdTypeOf[bool]() {
+			parsedParams[parameterName] = CmdParam{
+				ParamValue: "true",
+				ParamType: CmdTypeOf[bool](),
+			}
 			// all other params are treated as strings
 		} else {
 			// iterate it to get next word, should be value as current param is not bool type
@@ -54,7 +65,10 @@ func parseParams(listCmd []string, specificCmd CmdCommand) (map[string]string, e
 			}
 			// check if the next index is another param instead of a value
 			parameterValue := listCmd[it]
-			parsedParams[parameterName] = parameterValue
+			parsedParams[parameterName] = CmdParam{
+				ParamValue: parameterValue,
+				ParamType: CmdTypeOf[bool](),
+			}
 		}
 	}
 
@@ -77,37 +91,44 @@ const errorWrapFormatString = "ERROR: %w"
 // Description of a command, built up by the user and passed in to init inside of a map
 // Parameters field is used for easy error handling,
 type CmdCommand struct {
-	Handler     func(map[string]string) error
+	CmdName     string
+	Handler     func(map[string]CmdParam) error
 	Parameters  map[string]CmdParam
 	HelpMessage string
 }
 
 type CmdParam struct {
-	ParamName string
 	ParamType reflect.Type
 	ParamHelp string
+	ParamValue string
 }
 
 type cmdApp struct {
+	AppName            string
 	registeredCommands map[string]CmdCommand
 	helpMessage        string
 }
 
-// TODO 
 func (cmd CmdCommand) CmdHelp() {
 
-	// loop through all of the commands
+	fmt.Println("Params:")
+
+	for paramName, param := range cmd.Parameters {
+		fmt.Printf("  %-20s %s\n", paramName, param.ParamHelp)
+	}
 }
 
-// TODO 
-func (app cmdApp) AppHelp() error {
+func (app cmdApp) AppHelp() {
 
-	// Get the CmdCommand associated with cmdName
-	// if cmd, ok := app.registeredCommands[cmdName]; ok {
-	//
-	// }
-
-	return nil
+	if len(app.registeredCommands) == 1 {
+		fmt.Printf("\nUsage: %s [params]\n\n", os.Args[0])
+		app.registeredCommands[CMD_MAIN].CmdHelp()
+	} else {
+		for _, v := range app.registeredCommands {
+			fmt.Printf("\nUsage: %s %s [params]\n\n", os.Args[0], v.CmdName)
+			v.CmdHelp()
+		}
+	}
 }
 
 func (app cmdApp) GetregisteredCommands() map[string]CmdCommand {
@@ -115,14 +136,14 @@ func (app cmdApp) GetregisteredCommands() map[string]CmdCommand {
 }
 
 func CmdInitApp(appHelp string) cmdApp {
-		return cmdApp{
-			registeredCommands: make(map[string]CmdCommand),
-			helpMessage:        appHelp,
-		}
+	return cmdApp{
+		registeredCommands: make(map[string]CmdCommand),
+		helpMessage:        appHelp,
+	}
 }
 
-func (app cmdApp) RegisterCommand(cmdName string, cmd CmdCommand) {
-	app.registeredCommands[cmdName] = cmd
+func (app cmdApp) RegisterCommand(cmd CmdCommand) {
+	app.registeredCommands[cmd.CmdName] = cmd
 }
 
 // Given a command string (un trimmed, no cleanups etc.) run the associated handler from registeredCommands
@@ -152,7 +173,7 @@ func (app cmdApp) ProcessCommand(rawCmd string) error {
 		}
 	}
 
-	var parsedParams map[string]string
+	var parsedParams map[string]CmdParam
 	if len(app.registeredCommands) == 1 {
 		parsedParams, err = parseParams(listCmd, specificCmd)
 	} else {
@@ -160,6 +181,16 @@ func (app cmdApp) ProcessCommand(rawCmd string) error {
 	}
 	if err != nil {
 		return err
+	}
+
+	// check if help is flagged, if it is print help and return
+	for _, param := range parsedParams {
+		if param.ParamType == CmdTypeOf[CMD_HELP_TYPE]() {
+
+			app.AppHelp()
+
+			return nil
+		}
 	}
 
 	// run the command if not nil
--- cmd_test.go
+++ cmd_test.go
@@ -16,15 +16,15 @@ import (
 func Test_CmdProcess(t *testing.T) {
 	cmdApp := CmdInitApp("Welcome to cmdApp testing")
 	// add a dummy command
-	cmdApp.RegisterCommand(CMD_MAIN, CmdCommand{
-		Handler: func(params map[string]string) error {
-			fmt.Printf("%v", params)
-			fmt.Println("Poggers")
+	cmdApp.RegisterCommand(CmdCommand{
+		CmdName: CMD_MAIN,
+		Handler: func(params map[string]CmdParam) error {
+			fmt.Println("Poggers1")
 			return nil
 		},
 		Parameters: map[string]CmdParam{
 			"--help": {
-				ParamType: CmdTypeOf[bool](),
+				ParamType: CmdTypeOf[CMD_HELP_TYPE](),
 				ParamHelp: "Helphelp",
 			},
 			"--foo": {
@@ -38,37 +38,29 @@ func Test_CmdProcess(t *testing.T) {
 		},
 		HelpMessage: "testhelp",
 	})
-	// cmdMap["test"] = CmdCommand{
-	// 	Handler: func(params map[string]string) error {
-	// 		fmt.Printf("%v", params)
-	// 		fmt.Println("Poggers2")
-	// 		return nil
-	// 	},
-	// 	Parameters: map[string]CmdParamMetadata{
-	// 		"--foo2": {
-	// 			ParamType: CmdTypeOf[string](),
-	// 			ParamHelp: "Helpfoo",
-	// 		},
-	// 		"--bar2": {
-	// 			ParamType: CmdTypeOf[bool](),
-	// 			ParamHelp: "Helpbar",
-	// 		},
-	// 	},
-	// 	HelpMessage: "testhelp2",
-	// }
-	// cmdMap["test2"] = CmdCommand {
-	// 	CommandName: "test2",
-	// 	Handler: func(map[string]string) error {
-	// 		fmt.Println("Poggers2")
-	// 		return nil
-	// 	},
-	// 	Parameters: map[string]reflect.Type{
-	// 		"--foo": CmdTypeOf[string](),
-	// 		"--bar": CmdTypeOf[bool](),
-	// 	},
-	// 	HelpMessage: "testhelp",
-	// }
-	err := cmdApp.ProcessCommand("--help --foo=test --bar")
+	cmdApp.RegisterCommand(CmdCommand{
+		CmdName: "test",
+		Handler: func(params map[string]CmdParam) error {
+			fmt.Println("Poggers2")
+			return nil
+		},
+		Parameters: map[string]CmdParam{
+			"--help": {
+				ParamType: CmdTypeOf[CMD_HELP_TYPE](),
+				ParamHelp: "Helphelp",
+			},
+			"--foo": {
+				ParamType: CmdTypeOf[string](),
+				ParamHelp: "Helpfoo",
+			},
+			"--bar": {
+				ParamType: CmdTypeOf[bool](),
+				ParamHelp: "Helpbar",
+			},
+		},
+		HelpMessage: "testhelp",
+	})
+	err := cmdApp.ProcessCommand("test --help --foo=test --bar")
 	// TODO: add support for quotes!
 	// err := CmdProcess("--help --foo='test' --bar")
 	if err != nil {
